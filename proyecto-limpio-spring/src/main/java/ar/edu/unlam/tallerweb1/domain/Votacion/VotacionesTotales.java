2a8e1415a69f5fda8ab22195a3b2a325257d1985
estebanramz <esteramirez@alumno.unlam.edu.ar>
2022-11-23 22:57:43 -0300

    Agrego la clase votaciones totales

---
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/domain/Votacion/VotacionesTotales.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.tallerweb1.domain.Votacion;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VotacionesTotales {
+    Integer cantidadVotos;
+    String libro;
+}
